1fa9bc2f6dce9a182bf883bb699bd336a1f75b48
Christopher Reese <chris@reesenet.org>
2023-10-07 23:34:13 -0700

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- VisionPro_Fun/ContentView.swift
+++ VisionPro_Fun/ContentView.swift
@@ -69,40 +69,6 @@ struct ContentView: View {
                 }
             }
         }
-            
-        
-/*
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
-
-            Toggle("Show Immersive Space", isOn: $showImmersiveSpace)
-                .toggleStyle(.button)
-                .padding(.top, 50)
-        }
-        .padding()
-        .onChange(of: showImmersiveSpace) { _, newValue in
-            Task {
-                if newValue {
-                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
-                    case .opened:
-                        immersiveSpaceIsShown = true
-                    case .error, .userCancelled:
-                        fallthrough
-                    @unknown default:
-                        immersiveSpaceIsShown = false
-                        showImmersiveSpace = false
-                    }
-                } else if immersiveSpaceIsShown {
-                    await dismissImmersiveSpace()
-                    immersiveSpaceIsShown = false
-                }
-            }
-          
-        }
-*/
     }
 }
 
